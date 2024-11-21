10f59c7ddc31d3a7cd08182dad4249c670a88876
stefanmzeidler <stefanmzeidler@gmail.com>
2024-11-20 18:46:14 -0600

    Updated smor.py with a wrapper class, run method, and changed how directory is located.
    Wrapper allows instantation and use of run method to mirror compsplitwrapper.py. Directory for lib files needed by smor.py now determined by location of the file itself, so that smor.py can be run from a different directory.

--- SMOR/smor.py
+++ SMOR/smor.py
@@ -2,39 +2,55 @@ import os
 import subprocess
 import pandas as pd
 
-# testframe = pd.DataFrame({'compounds':['Donaudampfschifffahrtskapitänsmützenabzeichen','Abiturnote','Ablauforganisation','Einsatzfähigkeit','Heimspiel']})
 
-def smor(df):
-    prevdir = os.getcwd()
-    os.chdir(os.path.abspath('SMOR/'))
-    e = subprocess.Popen(('echo', df['compounds'].str.cat(sep='\n')), stdout=subprocess.PIPE)
-    output = subprocess.check_output(os.path.abspath('smor-infl'), stdin=e.stdout).decode('utf-8')
-    os.chdir(prevdir)
+class smor_wrapper:
+    def __init__(self, df):
+        self.df = df
 
-    subwords = []
-    newword = False
-    for line in output.splitlines():
-        if newword:
-            newword = False
-            segment = ''
-            wordSegments = []
-            insegment = True
-            for char in line:
-                if char == '<':
-                    insegment = False
-                    if len(segment) > 0:
-                        wordSegments.append(segment)
-                        segment = ''
-                elif insegment:
-                    segment += char
-                elif char == '>':
-                    insegment = True
-            subwords.append(wordSegments)
-        if line[:2] == '> ':
-            newword = True
+    def run(self):
+        return self.smor(self.df)
 
-    df.insert(len(df.columns), 'components', subwords)
-    return df
+    @staticmethod
+    def get_dir():
+        file_directory = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(file_directory, 'SMOR/')
 
-testframe = pd.DataFrame({'compounds':['Donaudampfschifffahrtskapitänsmützenabzeichen','Abiturnote','Ablauforganisation','Einsatzfähigkeit','Heimspiel']})
-print(smor(testframe))
+    @staticmethod
+    def smor(df):
+        prevdir = os.getcwd()
+        os.chdir(smor_wrapper.get_dir())
+        # os.chdir(os.path.abspath('SMOR/'))
+        e = subprocess.Popen(('echo', df['compounds'].str.cat(sep='\n')), stdout=subprocess.PIPE)
+        output = subprocess.check_output(os.path.abspath('smor-infl'), stdin=e.stdout).decode('utf-8')
+        os.chdir(prevdir)
+
+        subwords = []
+        newword = False
+        for line in output.splitlines():
+            if newword:
+                newword = False
+                segment = ''
+                wordSegments = []
+                insegment = True
+                for char in line:
+                    if char == '<':
+                        insegment = False
+                        if len(segment) > 0:
+                            wordSegments.append(segment)
+                            segment = ''
+                    elif insegment:
+                        segment += char
+                    elif char == '>':
+                        insegment = True
+                subwords.append(wordSegments)
+            if line[:2] == '> ':
+                newword = True
+
+        df.insert(len(df.columns), 'components', subwords)
+        return df
+
+
+testframe = pd.DataFrame({'compounds': ['Donaudampfschifffahrtskapitänsmützenabzeichen', 'Datensatz',
+                                        'Ablauforganisation', 'Einsatzfähigkeit', 'Heimspiel']})
+model_2 = smor_wrapper(testframe)
+print(model_2.run())
